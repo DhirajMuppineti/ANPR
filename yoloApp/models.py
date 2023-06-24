9eea81d8dd9740409b4a44a8d6aa0bfcf9e55c92
DhirajMuppineti <52035805+DhirajMuppineti@users.noreply.github.com>
2023-06-24 11:24:53 +0530

    Update models.py

--- yoloApp/models.py
+++ yoloApp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-
+class MultipleImage(models.Model):
+    car = models.FileField()
+    segmentedImg = models.TextField(blank=True)
+    plate_number = models.CharField(max_length=15)

cdb4eb6b4c7533254967d0811b2196a07f248105
DhirajMuppineti <52035805+DhirajMuppineti@users.noreply.github.com>
2023-06-24 11:27:46 +0530

    Update settings.py

--- anpr/settings.py
+++ anpr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'yoloApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -66,6 +68,9 @@ TEMPLATES = [
         },
     },
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 WSGI_APPLICATION = 'anpr.wsgi.application'
 

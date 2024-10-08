7428224002ff2735c8161fd3d8895fc6a7a3abd1
Dheeraj Muppineti <52035805+DhirajMuppineti@users.noreply.github.com>
2024-10-08 16:31:44 +0530

    Update settings.py

--- anpr/settings.py
+++ anpr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sl0%v^^lysvg=aqy*lnzqdk*yk5zg8+1n5v=7z^)madha(pr_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
